3b60a80bcae7eee10fe6572a7601a9bdb2373675
islammedhat90 <islam.medhat.h@gmail.com>
2023-10-05 08:48:57 -0700

    latest

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys
+    from sys import argv
+
+    arg_len = len(argv)
     sum = 0
-    for i in range(1, len(sys.argv)):
-        sum = sum + int(sys.argv[i])
+
+    if arg_len == 1:
+        sum = 0
+    else:
+        for index in range(1, arg_len):
+            sum += int(argv[index])
+
+    print("{:d}".format(sum))
